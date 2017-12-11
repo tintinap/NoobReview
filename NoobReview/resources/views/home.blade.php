bd7b2c323611ee02dcf26b2acae4a2465860af57
Tinnapat Plangsri <pltinnapat@gmail.com>
2017-12-12 03:49:23 +0700

    update home.blade.php
    still have a bug with last slide

--- NoobReview/resources/views/home.blade.php
+++ NoobReview/resources/views/home.blade.php
@@ -1,165 +1,115 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>NOOB REVIEW</title>
-    <style>
-    /*Start Navbar*/
-    body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 body, html {
     height: 100%;
     line-height: 1.8;
 }
-/* Full height image header */
-.bgimg-1 {
-    background-position: center;
-    background-size: cover;
-    background-image: url("/w3images/mac.jpg");
-    min-height: 100%;
-}
-.w3-bar .w3-button {
-    padding: 16px;
-}
-/*End Navbar*/
 
-/*Start Slide*/
-* {box-sizing:border-box}
-body {font-family: Verdana,sans-serif;}
-.mySlides {display:none}
-
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
+.center {
+    text-align: center;
 }
 
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 100px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-  color: white; text-shadow: black 0.1em 0.1em 0.2em;
+.pagination {
+    display: inline-block;
 }
 
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
+.pagination a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    transition: background-color .3s;
+    border: 1px solid #ddd;
+    margin: 0 4px;
 }
 
-/* The dots/bullets/indicators */
-.dot {
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
+.pagination a.active {
+    background-color: #454945;
+    color: white;
+    border: 1px solid #454945;
 }
 
-.active {
-  background-color: #717171;
+.pagination a:hover:not(.active) {background-color: #000000d9}
+
+a:link {
+    color: #000;
+    text-decoration: none;
 }
 
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
+a:hover {
+    color: #000;
+    text-decoration: none;
 }
 
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
+.picture:hover {
+    opacity: 0.3;
 }
 
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .text {font-size: 11px}
-}
 </style>
-</head> <!--Here is Head!!!!-->
-<body style="background-color:#F4F7F7;">
-    <!--Start Navbar-->
+<body style="background-color: #000000">
+
+<!--Start Navbar-->
         <!-- Navbar (sit on top) -->
-    <div class="container">
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-        <a href="/" class="w3-bar-item w3-button w3-wide">NOOB R</a>
+    <div class="w3-top">
+    <div class="w3-bar w3-black w3-card" id="myNavbar">
+        <a href="/" class="w3-bar-item w3-button w3-wide">NOOB-R</a>
         <a href="/series-list" class="w3-bar-item w3-button w3-wide">Series</a>
-    </nav>
+    </div>
     </div>
 <!--End Navbar-->
+<div style="height:650px !important;">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" style="height:650px; padding-top: 20px">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
 
-<div class="slideshow-container" align="center">
+    <!-- Wrapper for slides -->
 
-@foreach($series as $item)
-<div class="mySlides fade">
-  <div class="numbertext"></div>
-  <!-- link review page-->
-   <a href="/review/{{$item->ID}}">
-    <img src="{{ $item->cover }}" width="1000" height="600">
-    <div class="text"><strong>{{ $item->name }}</strong> </div>
-  </a>
-</div>
-@endforeach
+    <div class="carousel-inner">
+        <div class="item active" style="height:630px; padding-top: 20px">
+            @foreach ($series as $item)
+                <a href="/review/{{$item->ID}}"><img src="{{ $item->cover }}" style="width:100%;height:630px;"></a>
+            @endforeach
+        </div>
 
-</div>
+        <div class="item" style="height:630px; padding-top: 20px">
+            @foreach ($series as $item1)
+                <a href="/review/{{$item->ID}}"><img src="{{ $item->cover }}" style="width:100%;height:630px;"></a>
+            @endforeach
+        </div>
+        
+        <div class="item" style="height:630px !; padding-top: 20px">
+            <!-- @foreach ($series as $item) -->
+            <img src="https://ctd-thechristianpost.netdna-ssl.com/en/full/64805/gothamgotham-season-4.jpg?w=760&h=428" style="width:100%;height:630px;">
+            <!-- @endforeach -->
+        </div>
+    </div>
 
-
-<script>
-  var slideIndex = 0;
-  showSlides();
-
-  function showSlides() {
-      var i;
-      var slides = document.getElementsByClassName("mySlides");
-      var dots = document.getElementsByClassName("dot");
-      for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active";
-      setTimeout(showSlides, 3500); // Change image every 2 seconds
-  }
-</script>
-
-<!-- Footer -->
-<footer class="w3-center w3-black w3-padding-64">
-  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
-  <div class="w3-xlarge w3-section">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-</footer>
+</div>
+
 </body>
 </html>
