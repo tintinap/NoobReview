3cf377e81748037d512c5519f519997b676d8272
Tinnapat Plangsri <pltinnapat@gmail.com>
2017-12-12 01:41:17 +0700

    update series.blade.php

--- NoobReview/resources/views/series.blade.php
+++ NoobReview/resources/views/series.blade.php
@@ -37,16 +37,30 @@ body, html {
     border: 1px solid #454945;
 }
 
-.pagination a:hover:not(.active) {background-color: #000000;}
+.pagination a:hover:not(.active) {background-color: #000000d9}
+
+a:link {
+    color: #000;
+    text-decoration: none;
+}
+
+a:hover {
+    color: #000;
+    text-decoration: none;
+}
+
+.picture:hover {
+    opacity: 0.3;
+}
 
 </style>
-<body style="background-color: #F4F7F7">
+<body style="background-color: #ffffff">
 
     <!--Start Navbar-->
         <!-- Navbar (sit on top) -->
     <div class="w3-top">
-    <div class="w3-bar w3-white w3-card" id="myNavbar">
-        <a href="/" class="w3-bar-item w3-button w3-wide">NOOB R</a>
+    <div class="w3-bar w3-black w3-card" id="myNavbar">
+        <a href="/" class="w3-bar-item w3-button w3-wide">NOOB-R</a>
         <a href="/series-list" class="w3-bar-item w3-button w3-wide">Series</a>
     </div>
     </div>
@@ -65,24 +79,23 @@ body, html {
 <!-- <center> -->
   <div class="w3-row">
     @foreach ($row as $item)
-<div class="container">
-    <div class="w3-col s4 w3-blue w3-center">
-      <div class="gallery">
-        <a href="/review/{{ $item->ID }}">
-          <img src="{{ $item->icon }}" alt="Original" width="250" height="250">
-        </a>
-        <div class="desc"><br>{{ $item->name }}</div>
-      </div>
+    <div class="container">
+        <div class="w3-col s4 w3-white w3-center">
+        <div class="gallery">
+            <a href="/review/{{ $item->ID }}">
+                <img class="picture" src="{{ $item->icon }}" alt="Original" width="290" height="400">
+            </a>
+            <a href="/review/{{ $item->ID }}">
+                <div class="desc" style="color:#000!important"><br>{{ $item->name }}</div>
+            <br></a>
+        </div>
+        </div>
     </div>
-</div>
     @endforeach
   </div>
   <!-- </center> -->
     <hr>
 @endforeach
-
-
-
 
 <br>
 <br>
@@ -96,23 +109,9 @@ body, html {
     <a href="/series-list?page={{ $date->lastpage() }}"> &raquo; </a>
   </div>
 </div>
+<br>
+<br> 
 
-
-
-<!-- Footer -->
-<footer class="w3-center w3-black w3-padding-64">
-  <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
-  <div class="w3-xlarge w3-section">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
-  </div>
-  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-</footer>
- 
 </body>
 </html>
 
