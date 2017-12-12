029b61a1635c3f1f99e63934f9243e0d0695a2f8
Unknown <60070146@it.kmitl.ac.th>
2017-12-12 22:25:20 +0700

    WTF???

--- NoobReview/resources/views/contact.blade.php
+++ NoobReview/resources/views/contact.blade.php
@@ -76,41 +76,41 @@ a:hover {
     <br>
     <!--First-->
     <div class="row">
-    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/Rew.Tinnapat.Plangsri" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323167_2014213742130513_1352571139_n.jpg?oh=e8364d1ba979d776f5a8a69410755340&oe=5A309681" width="40
-        %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br> Name: Tinnapat Plangsri <br>ID: 60070028</p></div>
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/Rew.Tinnapat.Plangsri" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323167_2014213742130513_1352571139_n.jpg?oh=e8364d1ba979d776f5a8a69410755340&oe=5A309681" width="200
+        %" height="200%" class="img-circle"></a></div>
+    <div class="col-sm-6"><p style="font-size: 20px"><br><br> Name: Tinnapat Plangsri <br>ID: 60070028</p></div>
     </div>
 
     <br>
     <!--Second-->
     <div class="row">
-    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/pongsapak.pinyapong" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25287006_2014215338797020_211553747_n.jpg?oh=ec56397e33b9278cdf39b21069997690&oe=5A31B604" width="40
-        %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br> Name: Pongsapak Pinyapong <br>ID: 60070056</p></div>
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/pongsapak.pinyapong" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25287006_2014215338797020_211553747_n.jpg?oh=ec56397e33b9278cdf39b21069997690&oe=5A31B604" width="200
+        %" height="200%" class="img-circle"></a></div>
+    <div class="col-sm-6"><p style="font-size: 20px"><br><br> Name: Pongsapak Pinyapong <br>ID: 60070056</p></div>
     </div>
 
     <br>
     <!--Third-->
     <div class="row">
-    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/B.Kzania" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/20708398_1965749186976969_6707108658897089025_n.jpg?oh=5e94d0500f004938fbca53dcc283a8c3&oe=5AC434C1" width="40
-        %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br> Name: Benyapha Kheowchaoom <br>ID: 60070146</p></div>
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/B.Kzania" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/20708398_1965749186976969_6707108658897089025_n.jpg?oh=5e94d0500f004938fbca53dcc283a8c3&oe=5AC434C1" width="200
+        %" height="200%" class="img-circle"></a></div>
+    <div class="col-sm-6"><p style="font-size: 20px"><br><br> Name: Benyapha Kheowchaoom <br>ID: 60070146</p></div>
     </div>
 
     <br>
     <!--Fourth-->
     <div class="row">
-    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/benjamad.rittiwanich" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/19601420_1032445676890497_2595351139606470879_n.jpg?oh=0599a287aa13da8321f924227255b38c&oe=5AC4E0AA" width="40
-        %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br> Name: Benjamad Worthless <br>ID: 60070044</p></div>
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/benjamad.rittiwanich" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/19601420_1032445676890497_2595351139606470879_n.jpg?oh=0599a287aa13da8321f924227255b38c&oe=5AC4E0AA" width="200
+        %" height="200%" class="img-circle"></a></div>
+    <div class="col-sm-6"><p style="font-size: 20px"><br><br> Name: Benjamad Worthless <br>ID: 60070044</p></div>
     </div>
 
     <br>
     <!--Fifth-->
     <div class="row">
-    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/V.d.Zhukov" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323297_2014213875463833_1301321027_n.jpg?oh=362f493a5ebb6753aa2c673ee2fefa06&oe=5A3155A1" width="40
-        %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br> Name: Raksina Kanokwut <br>ID: 600700??</p></div>
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/V.d.Zhukov" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323297_2014213875463833_1301321027_n.jpg?oh=362f493a5ebb6753aa2c673ee2fefa06&oe=5A3155A1" width="200
+        %" height="200%" class="img-circle"></a></div>
+    <div class="col-sm-6"><p style="font-size: 20px"><br><br> Name: Raksina Kanokwut <br>ID: 60070082</p></div>
     </div>
     <br>
 </div>
