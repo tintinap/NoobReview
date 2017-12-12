b36ddd0800e618cc4aab79bb4d8f3adcd9e79388
Unknown <60070146@it.kmitl.ac.th>
2017-12-12 20:03:53 +0700

    แก้contact

--- NoobReview/resources/views/contact.blade.php
+++ NoobReview/resources/views/contact.blade.php
@@ -4,18 +4,11 @@
         <title>NOOB REVIEW</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
-    </head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
 <style>
 body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 body, html {
@@ -67,7 +60,7 @@ a:hover {
 <body style="background-color: #000000">
 
 <!--Start Navbar-->
-    <!-- Navbar (sit on top) -->
+        <!-- Navbar (sit on top) -->
     <div class="w3-top">
     <div class="w3-bar w3-black w3-card" id="myNavbar">
         <a href="/" class="w3-bar-item w3-button w3-wide">NOOB-R</a>
@@ -77,51 +70,49 @@ a:hover {
     </div>
 <!--End Navbar-->
 
-    <body style="background-color: #000000">
-    <div class="container" style="background-color: #ffffff">
-    <h1 align="center">Contact</h1>
+<div class="container" style="background-color: #ffffff">
+    <h1 align="center"><br><br> Contact</h1>
     <br>
     <br>
     <!--First-->
     <div class="row">
-    <div class="span6" align="center"><a href="https://www.facebook.com/Rew.Tinnapat.Plangsri" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323167_2014213742130513_1352571139_n.jpg?oh=e8364d1ba979d776f5a8a69410755340&oe=5A309681" width="40
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/Rew.Tinnapat.Plangsri" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323167_2014213742130513_1352571139_n.jpg?oh=e8364d1ba979d776f5a8a69410755340&oe=5A309681" width="40
         %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br><br><br> Name: Tinnapat Plangsri <br>ID: 60070028</p></div>
+    <div class="span6"><p style="font-size: 20px"><br><br> Name: Tinnapat Plangsri <br>ID: 60070028</p></div>
     </div>
 
     <br>
     <!--Second-->
     <div class="row">
-    <div class="span6" align="center"><a href="https://www.facebook.com/pongsapak.pinyapong" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25287006_2014215338797020_211553747_n.jpg?oh=ec56397e33b9278cdf39b21069997690&oe=5A31B604" width="40
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/pongsapak.pinyapong" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25287006_2014215338797020_211553747_n.jpg?oh=ec56397e33b9278cdf39b21069997690&oe=5A31B604" width="40
         %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br><br><br> Name: Pongsapak Pinyapong <br>ID: 60070056</p></div>
+    <div class="span6"><p style="font-size: 20px"><br><br> Name: Pongsapak Pinyapong <br>ID: 60070056</p></div>
     </div>
 
     <br>
     <!--Third-->
     <div class="row">
-    <div class="span6" align="center"><a href="https://www.facebook.com/B.Kzania" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/20708398_1965749186976969_6707108658897089025_n.jpg?oh=5e94d0500f004938fbca53dcc283a8c3&oe=5AC434C1" width="40
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/B.Kzania" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/20708398_1965749186976969_6707108658897089025_n.jpg?oh=5e94d0500f004938fbca53dcc283a8c3&oe=5AC434C1" width="40
         %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br><br><br> Name: Benyapha Kheowchaoom <br>ID: 60070146</p></div>
+    <div class="span6"><p style="font-size: 20px"><br><br> Name: Benyapha Kheowchaoom <br>ID: 60070146</p></div>
     </div>
 
     <br>
     <!--Fourth-->
     <div class="row">
-    <div class="span6" align="center"><a href="https://www.facebook.com/benjamad.rittiwanich" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/19601420_1032445676890497_2595351139606470879_n.jpg?oh=0599a287aa13da8321f924227255b38c&oe=5AC4E0AA" width="40
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/benjamad.rittiwanich" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/19601420_1032445676890497_2595351139606470879_n.jpg?oh=0599a287aa13da8321f924227255b38c&oe=5AC4E0AA" width="40
         %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br><br><br> Name: Benjamad Worthless <br>ID: 60070044</p></div>
+    <div class="span6"><p style="font-size: 20px"><br><br> Name: Benjamad Worthless <br>ID: 60070044</p></div>
     </div>
 
     <br>
     <!--Fifth-->
     <div class="row">
-    <div class="span6" align="center"><a href="https://www.facebook.com/V.d.Zhukov" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323297_2014213875463833_1301321027_n.jpg?oh=362f493a5ebb6753aa2c673ee2fefa06&oe=5A3155A1" width="40
+    <div class="col-sm-6" align="center"><a href="https://www.facebook.com/V.d.Zhukov" target="_blank"><img src="https://scontent.fbkk5-2.fna.fbcdn.net/v/t34.0-12/25323297_2014213875463833_1301321027_n.jpg?oh=362f493a5ebb6753aa2c673ee2fefa06&oe=5A3155A1" width="40
         %" height="40%" class="img-circle"></a></div>
-    <div class="span6"><p style="font-size: 20px"><br><br><br><br> Name: Raksina Kanokwut <br>ID: 60070082</p></div>
+    <div class="span6"><p style="font-size: 20px"><br><br> Name: Raksina Kanokwut <br>ID: 600700??</p></div>
     </div>
+    <br>
 </div>
-
-
     </body>
 </html>
